909db8a91f1b57baba9c68fecb77073a73d0572c
GiXross <93394637+GiXross@users.noreply.github.com>
2025-01-15 14:17:55 -0300

    Simulated Annealing atualizado com X
    Simulated Annealing atualizado com X variando de [0,1] aleatoriamente

--- simulatedAnnealing.py
+++ simulatedAnnealing.py
@@ -2,7 +2,8 @@ from math import exp
 from readFromFile import readProblem
 from randomFunction import randomSolution
 from random import randint
-
+from random import random
+import time
 
 def temperaturaInicial(beta, gama, SAmax, to, s,dictVert,upperBound):
     t = to
@@ -15,7 +16,7 @@ def temperaturaInicial(beta, gama, SAmax, to, s,dictVert,upperBound):
             if delta<0:
                 aceitos += 1
             else:
-                x = 0.5
+                x = random()
                 if x < exp((-delta / t)):
                     aceitos +=1
         if aceitos >= gama*SAmax:
@@ -187,7 +188,7 @@ def simulAnnealing(s, to, SAmax, dictVert,upperBound, alfa):
                 if(funcCusto(sLinha, dictVert) < funcCusto(solOtima, dictVert)): #Se o novo s for menor do que a atual solução ótima
                     solOtima = sLinha
             else:
-                x = 0.5
+                x = random()
                 if x < exp((-delta/t)):
                     s = sLinha
         t = alfa * t
@@ -196,16 +197,213 @@ def simulAnnealing(s, to, SAmax, dictVert,upperBound, alfa):
     return s
 
 
-def main(SAmax, alfa, beta, gama,pE):
+def main(alfa, beta, gama,pE):
     numVertices,numArcos, numRecursos,lowerBound, upperBound,vetResourcesByVertices, dictVert = readProblem(pE)
-    visited, custoAcumulado, recursosAcumulados = randomSolution(numVertices, numArcos, numRecursos, lowerBound, upperBound, vetResourcesByVertices, dictVert)
-    to = temperaturaInicial(beta, gama,SAmax, 1,visited, dictVert, upperBound)
-    print('Temperatura Inicial: ',to)
-    melhorSolEncontrada = simulAnnealing(visited, to, SAmax, dictVert,upperBound, alfa)
-    print('Melhor Solução Encontrada pelo Simulated Annealing:',melhorSolEncontrada)
-    print('Custo Melhor Solucao Encontrada:', funcCusto(melhorSolEncontrada, dictVert))
-    print('Recursos Gastos Melhor Solucao Encontrada:', funcRecurso(melhorSolEncontrada, dictVert))
+    SAmax = numVertices
+    valTempo = 0
+    for i in range(10):
+        visited, custoAcumulado, recursosAcumulados = randomSolution(numVertices, numArcos, numRecursos, lowerBound,
+                                                                     upperBound, vetResourcesByVertices, dictVert)
+        tempo1 = time.time()
+        print('Solução %d' % (i + 1))
+        to = temperaturaInicial(beta, gama,SAmax, 1,visited, dictVert, upperBound)
+        print('Temperatura Inicial: ',to)
+        melhorSolEncontrada = simulAnnealing(visited, to, SAmax, dictVert,upperBound, alfa)
+        print('Melhor Solução Encontrada pelo Simulated Annealing:',melhorSolEncontrada)
+        print('Custo Melhor Solucao Encontrada:', funcCusto(melhorSolEncontrada, dictVert))
+        print('Recursos Gastos Melhor Solucao Encontrada:', funcRecurso(melhorSolEncontrada, dictVert))
+
+        tempo2 = time.time()
+
+        difTempo = (tempo2 - tempo1)
+        valTempo += difTempo
+        print('Tempo da execução: ', difTempo)
+    print('Média de tempo das execuções: ', valTempo / 10)
 
 if __name__ == '__main__':
-    problemaEscolhido = 1
-    main(SAmax=1000,alfa=0.9, beta=2,gama=0.95, pE=problemaEscolhido)
+    problemaEscolhido = 2
+
+    main(alfa=0.99, beta=2, gama=0.95, pE=problemaEscolhido)
+
+
+
+
+# Problema 2(com x pertence a [0,1])
+# Solução 1
+# Temperatura Inicial:  1024
+# Melhor Solução Encontrada pelo Simulated Annealing: [1, 72, 86, 74, 34, 100]
+# Custo Melhor Solucao Encontrada: 216
+# Recursos Gastos Melhor Solucao Encontrada: 36
+# Tempo da execução:  27.796696424484253
+# Solução 2
+# Temperatura Inicial:  1024
+# Melhor Solução Encontrada pelo Simulated Annealing: [1, 72, 86, 74, 34, 100]
+# Custo Melhor Solucao Encontrada: 216
+# Recursos Gastos Melhor Solucao Encontrada: 36
+# Tempo da execução:  27.129844665527344
+# Solução 3
+# Temperatura Inicial:  128
+# Melhor Solução Encontrada pelo Simulated Annealing: [1, 72, 86, 74, 34, 100]
+# Custo Melhor Solucao Encontrada: 216
+# Recursos Gastos Melhor Solucao Encontrada: 36
+# Tempo da execução:  24.023000478744507
+# Solução 4
+# Temperatura Inicial:  256
+# Melhor Solução Encontrada pelo Simulated Annealing: [1, 72, 86, 74, 34, 100]
+# Custo Melhor Solucao Encontrada: 216
+# Recursos Gastos Melhor Solucao Encontrada: 36
+# Tempo da execução:  25.352001428604126
+# Solução 5
+# Temperatura Inicial:  1
+# Melhor Solução Encontrada pelo Simulated Annealing: [1, 72, 86, 74, 34, 100]
+# Custo Melhor Solucao Encontrada: 216
+# Recursos Gastos Melhor Solucao Encontrada: 36
+# Tempo da execução:  15.523013353347778
+# Solução 6
+# Temperatura Inicial:  1
+# Melhor Solução Encontrada pelo Simulated Annealing: [1, 72, 86, 74, 34, 100]
+# Custo Melhor Solucao Encontrada: 216
+# Recursos Gastos Melhor Solucao Encontrada: 36
+# Tempo da execução:  15.663985967636108
+# Solução 7
+# Temperatura Inicial:  128
+# Melhor Solução Encontrada pelo Simulated Annealing: [1, 72, 86, 74, 34, 100]
+# Custo Melhor Solucao Encontrada: 216
+# Recursos Gastos Melhor Solucao Encontrada: 36
+# Tempo da execução:  24.417999505996704
+# Solução 8
+# Temperatura Inicial:  512
+# Melhor Solução Encontrada pelo Simulated Annealing: [1, 72, 86, 74, 34, 100]
+# Custo Melhor Solucao Encontrada: 216
+# Recursos Gastos Melhor Solucao Encontrada: 36
+# Tempo da execução:  25.95504856109619
+# Solução 9
+# Temperatura Inicial:  1
+# Melhor Solução Encontrada pelo Simulated Annealing: [1, 72, 86, 74, 34, 100]
+# Custo Melhor Solucao Encontrada: 216
+# Recursos Gastos Melhor Solucao Encontrada: 36
+# Tempo da execução:  14.922965288162231
+# Solução 10
+# Temperatura Inicial:  512
+# Melhor Solução Encontrada pelo Simulated Annealing: [1, 72, 86, 74, 34, 100]
+# Custo Melhor Solucao Encontrada: 216
+# Recursos Gastos Melhor Solucao Encontrada: 36
+# Tempo da execução:  25.012986421585083
+# Média de tempo das execuções:  22.579754209518434
+
+
+
+# Problema 2(com x podendo ser 0.1, 0.2, 0.3..., 1)
+# Solução 1
+# Temperatura Inicial:  65536
+# Melhor Solução Encontrada pelo Simulated Annealing: [1, 72, 86, 74, 34, 100]
+# Custo Melhor Solucao Encontrada: 216
+# Recursos Gastos Melhor Solucao Encontrada: 36
+# Tempo da execução:  38.72322702407837
+
+# Solução 2
+# Temperatura Inicial:  4096
+# Melhor Solução Encontrada pelo Simulated Annealing: [1, 72, 86, 74, 34, 100]
+# Custo Melhor Solucao Encontrada: 216
+# Recursos Gastos Melhor Solucao Encontrada: 36
+# Tempo da execução:  32.689213275909424
+
+# Solução 3
+# Temperatura Inicial:  8
+# Melhor Solução Encontrada pelo Simulated Annealing: [1, 72, 86, 74, 34, 100]
+# Custo Melhor Solucao Encontrada: 216
+# Recursos Gastos Melhor Solucao Encontrada: 36
+# Tempo da execução:  16.430323362350464
+
+# Solução 4
+# Temperatura Inicial:  2048
+# Melhor Solução Encontrada pelo Simulated Annealing: [1, 72, 86, 74, 34, 100]
+# Custo Melhor Solucao Encontrada: 216
+# Recursos Gastos Melhor Solucao Encontrada: 36
+# Tempo da execução:  29.09767723083496
+
+# Solução 5
+# Temperatura Inicial:  2048
+# Melhor Solução Encontrada pelo Simulated Annealing: [1, 72, 86, 74, 34, 100]
+# Custo Melhor Solucao Encontrada: 216
+# Recursos Gastos Melhor Solucao Encontrada: 36
+# Tempo da execução:  32.588782787323
+
+# Solução 6
+# Temperatura Inicial:  1
+# Melhor Solução Encontrada pelo Simulated Annealing: [1, 72, 86, 74, 34, 100]
+# Custo Melhor Solucao Encontrada: 216
+# Recursos Gastos Melhor Solucao Encontrada: 36
+# Tempo da execução:  15.94067645072937
+
+# Solução 7
+# Temperatura Inicial:  512
+# Melhor Solução Encontrada pelo Simulated Annealing: [1, 72, 86, 74, 34, 100]
+# Custo Melhor Solucao Encontrada: 216
+# Recursos Gastos Melhor Solucao Encontrada: 36
+# Tempo da execução:  28.346956491470337
+
+# Solução 8
+# Temperatura Inicial:  512
+# Melhor Solução Encontrada pelo Simulated Annealing: [1, 72, 86, 74, 34, 100]
+# Custo Melhor Solucao Encontrada: 216
+# Recursos Gastos Melhor Solucao Encontrada: 36
+# Tempo da execução:  27.609987497329712
+
+# Solução 9
+# Temperatura Inicial:  1024
+# Melhor Solução Encontrada pelo Simulated Annealing: [1, 72, 86, 74, 34, 100]
+# Custo Melhor Solucao Encontrada: 216
+# Recursos Gastos Melhor Solucao Encontrada: 36
+# Tempo da execução:  28.832045793533325
+
+# Solução 10
+# Temperatura Inicial:  32768
+# Melhor Solução Encontrada pelo Simulated Annealing: [1, 72, 86, 74, 34, 100]
+# Custo Melhor Solucao Encontrada: 216
+# Recursos Gastos Melhor Solucao Encontrada: 36
+# Tempo da execução:  38.01396441459656
+# Média de tempo das execuções:  28.82728543281555
+
+
+
+
+#Old
+# Solução 1
+# Temperatura Inicial:  256
+# Melhor Solução Encontrada pelo Simulated Annealing: [1, 72, 86, 74, 34, 100]
+# Custo Melhor Solucao Encontrada: 216
+# Recursos Gastos Melhor Solucao Encontrada: 36
+# Tempo da execução:  118.05201172828674
+
+# Solução 2
+# Temperatura Inicial:  4194304
+# Melhor Solução Encontrada pelo Simulated Annealing: [1, 72, 86, 74, 34, 100]
+# Custo Melhor Solucao Encontrada: 216
+# Recursos Gastos Melhor Solucao Encontrada: 36
+# Tempo da execução:  188.81798648834229
+
+
+# Solução 3
+# Temperatura Inicial:  512
+# Melhor Solução Encontrada pelo Simulated Annealing: [1, 72, 86, 74, 34, 100]
+# Custo Melhor Solucao Encontrada: 216
+# Recursos Gastos Melhor Solucao Encontrada: 36
+# Tempo da execução:  112.96100068092346
+
+# Solução 4
+# Temperatura Inicial:  2048
+# Melhor Solução Encontrada pelo Simulated Annealing: [1, 72, 86, 74, 34, 100]
+# Custo Melhor Solucao Encontrada: 216
+# Recursos Gastos Melhor Solucao Encontrada: 36
+# Tempo da execução:  134.82916164398193
+# Solução 5
+# Temperatura Inicial:  302231454903657293676544
+#
+
+#Solução 1 OLD
+# Temperatura Inicial:  8192
+# Melhor Solução Encontrada pelo Simulated Annealing: [1, 72, 86, 74, 34, 100]
+# Custo Melhor Solucao Encontrada: 216
+# Recursos Gastos Melhor Solucao Encontrada: 36
+# Tempo da execução:  264.42280197143555
